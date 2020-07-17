d67e4c2bfd80cdf7f6bdf6df20059c8024639401
joshhunt <josh@trtr.co>
2020-07-17 12:45:47 +0100

    Remove custom value converters, using ones from libs instead

--- GhostOverlay/XamlUtils.cs
+++ GhostOverlay/XamlUtils.cs
@@ -1,65 +1,11 @@
 using System;
-using System.Collections;
-using System.Collections.Specialized;
-using System.Globalization;
-using System.Linq;
 using Windows.UI;
-using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Markup;
 
 namespace GhostOverlay
 {
-    public class BooleanVisibilityConverter : IValueConverter
-    {
-        public object Convert(object value, Type targetType, object parameter, string language)
-        {
-            var input = System.Convert.ToBoolean(value);
-            var paramString = parameter as string;
-
-            if (paramString?.Equals("CollapsedWhenTrue") ?? false)
-            {
-                return input ? Visibility.Collapsed : Visibility.Visible;
-            }
-
-            return input ? Visibility.Visible : Visibility.Collapsed;
-        }
-
-        public object ConvertBack(object value, Type targetType,
-            object parameter, string language)
-        {
-            throw new NotImplementedException();
-        }
-    }
-
-    public class BooleanBrushConverter : IValueConverter
-    {
-        public object Convert(object value, Type targetType, object parameter, string language)
-        {
-            var colors = new[] {"#FF00FF00", "#FFFF0000"};
-            var paramString = parameter as string;
-            var input = System.Convert.ToBoolean(value);
-
-            if (!string.IsNullOrEmpty(paramString))
-            {
-                colors = paramString.Split(new char[] { '|' });
-            }
-
-            var resultColor = input ? colors[0] : colors[1];
-
-            Color x = (Color)XamlBindingHelper.ConvertValue(typeof(Color), resultColor);
-            return new SolidColorBrush(x);
-        }
-
-        public object ConvertBack(object value, Type targetType,
-            object parameter, string language)
-        {
-            throw new NotImplementedException();
-        }
-    }
-
     public class NumberFormatterConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, string language)
