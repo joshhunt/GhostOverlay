08c14b69c6e3b6976e457d27b3d7ee278fbb1ab8
joshhunt <josh@trtr.co>
2020-07-23 17:40:30 +0100

    Add base EqualHeightWrapPanel, copied from WindowsCommunityToolkit WrapPanel

---
+++ GhostOverlay/Views/EqualHeightWrapPanel/Data.cs
@@ -0,0 +1,38 @@
+﻿// Licensed to the.NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Windows.UI.Xaml.Controls;
+
+namespace GhostOverlay
+{
+    /// <summary>
+    /// WrapPanel is a panel that position child control vertically or horizontally based on the orientation and when max width/ max height is recieved a new row(in case of horizontal) or column (in case of vertical) is created to fit new controls.
+    /// </summary>
+    public partial class EqualHeightWrapPanel
+    {
+        [System.Diagnostics.DebuggerDisplay("U = {U} V = {V}")]
+        private struct UvMeasure
+        {
+            internal static readonly UvMeasure Zero = default(UvMeasure);
+
+            internal double U { get; set; }
+
+            internal double V { get; set; }
+
+            public UvMeasure(Orientation orientation, double width, double height)
+            {
+                if (orientation == Orientation.Horizontal)
+                {
+                    U = width;
+                    V = height;
+                }
+                else
+                {
+                    U = height;
+                    V = width;
+                }
+            }
+        }
+    }
+}
